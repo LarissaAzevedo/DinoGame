f29ac5f542f0ce168f2077c10464e858ed8a387e
Lari Azevedo~ <larissa.santosazevedo@hotmail.com>
2022-05-14 20:36:08 -0300

    feat: basics moves

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# DinoGame
+# 🦖 Dino Game by LarissaAzevedo
+
+The Chrome's Dinosaur Game.
+
+## 👾 Play here:
+_Links coming soon..._
+
+
+## 🕹 Test here:
+
+_Images coming soon..._
+
+## 🛠 Getting started
+
+```sh
+git clone meurepo
+```
+
+## Features
---
+++ index.js
@@ -0,0 +1,63 @@
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+
+canvas.width = 1024;
+canvas.height = 576;
+
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+class Sprite {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.height = 150;
+  }
+
+  draw() {
+    context.fillStyle = "red";
+    context.fillRect(this.position.x, this.position.y, 50, this.height);
+  }
+
+  update() {
+    this.draw();
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.velocity.y + this.height >= canvas.height) {
+      this.velocity.y = 0;
+    }
+  }
+}
+
+const player = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  velocity: {
+    x: 0,
+    y: 10,
+  },
+});
+
+const enemy = new Sprite({
+  position: {
+    x: 400,
+    y: 100,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+});
+
+console.log("player :>> ", player);
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  context.fillStyle = "black";
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+  enemy.update();
+}
+
+animate();
